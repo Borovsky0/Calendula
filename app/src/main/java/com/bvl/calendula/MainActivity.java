81d59b2c3c3d1e0ac039af18e9e2df4892bb704c
Ivan <borovsky2000@gmail.com>
2021-10-28 22:17:26 +0300

    Initial commit

---
+++ app/src/main/java/com/bvl/calendula/MainActivity.java
@@ -0,0 +1,53 @@
+package com.bvl.calendula;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.bvl.calendula.ui.DayFragment;
+import com.bvl.calendula.ui.WeekFragment;
+import com.bvl.calendula.ui.MonthFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment selectedFragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(listener);
+        if(selectedFragment == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DayFragment()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    switch (item.getItemId())
+                    {
+                        case R.id.day:
+                            selectedFragment = new DayFragment();
+                            break;
+                        case R.id.week:
+                            selectedFragment = new WeekFragment();
+                            break;
+                        case R.id.month:
+                            selectedFragment = new MonthFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
---
+++ app/src/main/java/com/bvl/calendula/ui/MonthFragment.java
@@ -0,0 +1,20 @@
+package com.bvl.calendula.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bvl.calendula.R;
+
+public class MonthFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_month, container, false);
+    }
+}
