9c97ef5f0cd8087348fa25528612c9bd27744581
Anton Laptev <Laptev.likt590@gmail.com>
2021-12-01 23:37:57 +0300

    Added appearance of data in DayFragment

--- app/src/main/java/com/bvl/calendula/ScrollAdapter.java
+++ app/src/main/java/com/bvl/calendula/ScrollAdapter.java
@@ -13,14 +13,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.LocalDate;
 
 public class ScrollAdapter extends RecyclerView.Adapter<ScrollAdapter.ViewHolder> {
-
-    public ScrollAdapter (){
+    private OnDateClickListener onDateClickListener;
+    
+    public ScrollAdapter(OnDateClickListener onDateClickListener){
+        this.onDateClickListener = onDateClickListener;
     }
 
     @Override
     public ScrollAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.element_scroll, parent, false);
-        return new ViewHolder(rowItem);
+        return new ViewHolder(rowItem, onDateClickListener);
     }
 
     //@RequiresApi(api = Build.VERSION_CODES.O)
@@ -42,17 +44,26 @@ public class ScrollAdapter extends RecyclerView.Adapter<ScrollAdapter.ViewHolder
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView datetext;
         private TextView monthtext;
-
-        public ViewHolder(View view) {
+        
+        private OnDateClickListener onDateClickListener;
+        
+        public ViewHolder(View view, OnDateClickListener onDateClickListener) {
             super(view);
             view.setOnClickListener(this);
             this.datetext = view.findViewById(R.id.date);
             this.monthtext = view.findViewById(R.id.month);
+            this.onDateClickListener = onDateClickListener;
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(), "position : " + getLayoutPosition() + " text : " + this.datetext.getText(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(view.getContext(), "position : " + getLayoutPosition() + " text : " +
+                    this.datetext.getText() + " month: " + this.monthtext.getText(), Toast.LENGTH_SHORT).show();
+            onDateClickListener.onDateClick(getAdapterPosition());
         }
+    }
+    
+    public interface OnDateClickListener {
+        void onDateClick(int position);
     }
 }
--- app/src/main/java/com/bvl/calendula/ui/DayFragment.java
+++ app/src/main/java/com/bvl/calendula/ui/DayFragment.java
@@ -21,9 +21,11 @@ import com.bvl.calendula.ScrollAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-public class DayFragment extends Fragment {
+public class DayFragment extends Fragment implements ScrollAdapter.OnDateClickListener {
 
     RecyclerView recyclerView, scroll;
     FloatingActionButton button;
@@ -49,7 +51,7 @@ public class DayFragment extends Fragment {
         button = v.findViewById(R.id.add_button);
         scroll = v.findViewById(R.id.scroll);
         scroll.setLayoutManager(new LinearLayoutManager(DayFragment.this.getActivity(), RecyclerView.HORIZONTAL, false));
-        scroll.setAdapter(new ScrollAdapter());
+        scroll.setAdapter(new ScrollAdapter(this));
         scroll.getLayoutManager().scrollToPosition(Integer.MAX_VALUE / 2);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -73,7 +75,7 @@ public class DayFragment extends Fragment {
         pic_note = new ArrayList<>();
         audio_note = new ArrayList<>();
 
-        toArrays();
+        toArrays(cDate);
 
         adapter = new ElementAdapter(DayFragment.this.getActivity(),
                 id, name, date, day_of_week, periodicity, time_start,
@@ -84,24 +86,45 @@ public class DayFragment extends Fragment {
         return v;
     }
 
-    void toArrays(){
-        Cursor cursor = db_helper.read(cDate);
+    void toArrays(Date desiredDate){
+        Cursor cursor = db_helper.read(desiredDate);
         if(cursor.getCount() != 0)
         {
             while(cursor.moveToNext())
             {
-                id.add(cursor.getString(0));
-                name.add(cursor.getString(1));
-                date.add(cursor.getString(2));
-                day_of_week.add(cursor.getString(3));
-                periodicity.add(cursor.getString(4));
-                time_start.add(cursor.getString(5));
-                time_finish.add(cursor.getString(6));
-                tags.add(cursor.getString(7));
-                text_note.add(cursor.getString(8));
-                pic_note.add(cursor.getString(9));
-                audio_note.add(cursor.getString(10));
+                this.id.add(cursor.getString(0));
+                this.name.add(cursor.getString(1));
+                this.date.add(cursor.getString(2));
+                this.day_of_week.add(cursor.getString(3));
+                this.periodicity.add(cursor.getString(4));
+                this.time_start.add(cursor.getString(5));
+                this.time_finish.add(cursor.getString(6));
+                this.tags.add(cursor.getString(7));
+                this.text_note.add(cursor.getString(8));
+                this.pic_note.add(cursor.getString(9));
+                this.audio_note.add(cursor.getString(10));
             }
         }
+    }
+
+    @Override
+    public void onDateClick(int position) {
+        this.id.clear();
+        this.name.clear();
+        this.date.clear();
+        this.day_of_week.clear();
+        this.periodicity.clear();
+        this.time_start.clear();
+        this.time_finish.clear();
+        this.tags.clear();
+        this.text_note.clear();
+        this.pic_note.clear();
+        this.audio_note.clear();
+        int dif = Integer.MAX_VALUE / 2 - position;
+        //Calendar date = new GregorianCalendar(2021, 11, 3);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -dif);
+        toArrays(calendar.getTime());
+        adapter.notifyDataSetChanged();
     }
 }
